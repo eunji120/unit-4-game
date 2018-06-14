4ebc5c59fc5cea2e03946de93965856c5f5480f0
eunji120 <eunji120@gmail.com>
2018-06-13 22:42:23 -0400

    Edited files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
             whiel(numbers.length < 4); {
                 var computerNumber = Math.ceil(Math.random()*12)
                 var found = false;
+                //each round continues until the player's number equals the computerNumber
                 for (var i = 0; i < numbers.length; i++) {
                     if (numbers[i] === computerNumber) {
                         found = true; break
@@ -38,10 +39,11 @@ $(document).ready(function() {
     }
 
     function newGame() {
-
+        //after each round, the value of counter goes back to 0
         counter = 0;
         $('#yourScore').text(counter);
 
+        //randomly generated computerAnswer 
         function randomIntFromInterval(min,max) {
             return Math.floor(Math.random()*(max-min+1)+min);
         }
@@ -53,20 +55,28 @@ $(document).ready(function() {
 
             $('#yourScore').text(counter);
 
-            if (counter == computerAnswer) {
+            //when the player's number and the computerAnswer matches,
+            if (counter === computerAnswer) {
+                //'You won!' shows up
                 $('#status').text('You won!');
+                //the number of Wins gies up by 1
                 wins ++;
                 $('#win').text(wins);
                 console.log(wins)
+                //reset to a new round
                 $('#crystals').empty();
                 newCrystals();
                 newGame;
 
+            //when the player's number goes over the computerAnswer, 
             } else if (counter > computerAnswer) {
+                //'You lost!' shows up 
                 $('#status').text('You lost!')
+                //the number of Losses goes up by 1
                 losses ++;
                 $('#loss').text(losses);
                 console.log(losses)
+                //reset to a new round 
                 $('#crystals').empty();
                 newCrystals();
                 newGame();
